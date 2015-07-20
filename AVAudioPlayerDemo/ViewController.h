69b2821ea2e5c1aee6a1b94e9c64ba607399dee4
Richey <liqi1234@foxmail.com>
2015-07-20 15:13:57 +0800

    Initial Commit

---
+++ AVAudioPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVAudioPlayerDemo
+//
+//  Created by Richey on 15/7/20.
+//  Copyright (c) 2015年 Richey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AVAudioPlayerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AVAudioPlayerDemo
+//
+//  Created by Richey on 15/7/20.
+//  Copyright (c) 2015年 Richey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
